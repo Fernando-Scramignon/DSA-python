a1605747fef0b295ef2a167862f761d030ec8d7f
Fernando-Scramignon <scramignonnarde@gmail.com>
2023-02-02 16:52:11 -0300

    feat: starts the creation of the linked list class

---
+++ data_structures/linked_list/LinkedList.py
@@ -0,0 +1,39 @@
+import unittest
+
+class Node():
+    def __init__(self, element):
+        self.element = element
+        self.next = None
+
+class LinkedList(unittest.TestCase):
+    def push(element):
+        pass
+    
+    def insert(element, index):
+        pass
+
+    def get_element_at(index):
+        pass
+
+    def remove(element):
+        pass
+    
+    def index_of(element):
+        pass
+
+    def remove_at(index):
+        pass
+
+    def is_empty():
+        pass
+
+    def size():
+        pass
+
+    def toString():
+        pass
+
+    def __init__(self):
+        self._head = None
+        self._count = 0
+        
