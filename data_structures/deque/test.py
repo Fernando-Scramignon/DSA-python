f1bd8b5c0ceb45bf7dd8eb248bb4d85162c35f3c
Fernando-Scramignon <scramignonnarde@gmail.com>
2023-02-16 17:08:12 -0300

    refactor: adds types to variables and returns

--- data_structures/deque/test.py
+++ data_structures/deque/test.py
@@ -1,26 +1,24 @@
 import unittest
 
 from data_structures.deque.Deque import Deque
-import pdb
-
 
 class TestAddBack(unittest.TestCase):
     def setUp(self) -> None:
-        self.names = ['John', 'Jack', 'Ryan']
-        self.names_deque = Deque()
+        self.names: tuple = ('John', 'Jack', 'Ryan')
+        self.names_deque: Deque = Deque()
     
-    def test_add_when_empty(self):
-        names_deque = self.names_deque
-        names = self.names
+    def test_add_when_empty(self) -> None:
+        names_deque: Deque = self.names_deque
+        names: tuple = self.names
 
         names_deque.add_back(names[0])
 
         self.assertEqual(names_deque._items[0], self.names[0])
         
 
-    def test_add_normally(self):
-        names_deque = self.names_deque
-        names = self.names
+    def test_add_normally(self) -> None:
+        names_deque: Deque = self.names_deque
+        names: tuple = self.names
 
         for name in names:
             names_deque.add_back(name)
@@ -28,5 +26,6 @@ class TestAddBack(unittest.TestCase):
         for index, name in enumerate(names):
             self.assertEqual(names_deque._items[index], self.names[index])
 
-        
-        
+class TestAddFront(unittest.TestCase):
+    def setUp(self) -> None:
+        pass
