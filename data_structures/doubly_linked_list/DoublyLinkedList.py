a4b347287241fd9bf0a04bbc8e2f39c460508be9
Fernando-Scramignon <scramignonnarde@gmail.com>
2023-02-22 10:36:08 -0300

    feat: creates insert method of doubly linked list

--- data_structures/doubly_linked_list/DoublyLinkedList.py
+++ data_structures/doubly_linked_list/DoublyLinkedList.py
@@ -4,12 +4,51 @@ from data_structures.linked_list.LinkedList import LinkedList
 class DoublyNode(Node):
     def __init__(self, element: any) -> None:
         super().__init__(element)
-        self._prev: DoublyNode | None = None
+        self.prev: DoublyNode | None = None
 
 class DoublyLinkedList(LinkedList):
-    # check if the index is valid, if not, return None
     def insert(self, element: any, index: int) -> any:
-        pass
+        if index < 0 or index > self._count:
+            return False
+        
+        node: DoublyNode = DoublyNode(element)
+
+        if index == 0:
+            if not self._head:
+                self._head = node
+                self._tail = node
+                self._count += 1
+
+                return True
+            
+            old_head: DoublyNode = self._head
+            old_head.prev = node
+            node.next = old_head            
+            self._head = node
+
+            self._count += 1
+            return True
+        
+        if index == self._count:
+            old_tail: DoublyNode = self._tail
+
+            old_tail.next = node
+            node.prev = old_tail
+            self._tail = node
+
+            self._count += 1
+            return True
+            
+        previous: DoublyNode = self.get_element_at(index - 1)
+        current: DoublyNode = previous.next
+
+        previous.next = node
+        current.prev = node
+        node.prev = previous
+        node.next = node
+
+        self._count += 1
+        return True
     
     def __init__(self) -> None:
         super().__init__()
